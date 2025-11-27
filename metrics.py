3b47f8ee10f504db9ab57fdd70734967321e6246
AdalCastilla <122295391+AdalCastilla@users.noreply.github.com>
2025-11-26 23:38:44 -0600

    clean

--- metrics.py
+++ metrics.py
@@ -6,21 +6,21 @@ import numpy as np
 
 
 def preprocess(frame):
-    # tu preprocesamiento
+    
     return frame
 
 
 def main():
-    # 1. Cargar tu modelo base
+    
     model = YOLO("bestperson.pt")
 
     metrics = model.val(data="F:\Adal\Descargas\Proyecto Vision Artificial\Person detection.v16i.yolov8\data.yaml")
 
-# 3) imprime los valores importantes
-    print("Precision:", metrics.box.mp)      # mean precision
-    print("Recall:   ", metrics.box.mr)      # mean recall
-    print("mAP50:    ", metrics.box.map50)   # mAP@0.5
-    print("mAP50-95: ", metrics.box.map)     # mAP@[.5:.95]
+
+    print("Precision:", metrics.box.mp)      
+    print("Recall:   ", metrics.box.mr)      
+    print("mAP50:    ", metrics.box.map50)   
+    print("mAP50-95: ", metrics.box.map)     
 
 
 if __name__ == "__main__":
